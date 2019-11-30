1f065ae08ff0987faadbe2e2669312eb8e313acf
yoshifoxor <kurmelsa5d@gmail.com>
2019-12-01 02:08:20 +0900

    Update sequelize-loader

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,12 +1,21 @@
 'use strict';
+const fs = require('fs');
 const Sequelize = require('sequelize');
-const config = require('../config');
-const sequelize = new Sequelize(
-  `postgres://${config.db.username}:${config.db.password}@localhost:5432/${config.db.database}`,
-  {
-    operatorsAliases: false
-  }
-);
+const db = { database: "schedule_arranger", username: "postgres", password: "" };
+
+const config = fs.existsSync('../config.js') ? require('../config.js') : { db };
+
+let sequelize;
+try {
+  sequelize = new Sequelize(
+    `postgres://${config.db.username}:${config.db.password}@localhost:5432/${config.db.database}`,
+    {
+      operatorsAliases: false
+    }
+  );
+} catch (error) {
+  console.error(error);
+}
 
 module.exports = {
   database: sequelize,
