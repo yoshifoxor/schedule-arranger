6d1c7e4c4f64b9557811edbe9a7bace8ded1cc8a
yoshifoxor <kurmelsa5d@gmail.com>
2019-12-01 04:35:18 +0900

    予定の一覧の表示

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ passport.use(new GitHubStrategy({
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
+var schedulesRouter = require('./routes/schedules');
 
 var app = express();
 app.use(helmet());
@@ -77,6 +78,7 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
+app.use('/schedules', schedulesRouter);
 
 app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }),
   function (req, res) {
--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,20 +2,19 @@
 const fs = require('fs');
 const Sequelize = require('sequelize');
 const db = { database: "schedule_arranger", username: "postgres", password: "" };
-
-const config = fs.existsSync('../config.js') ? require('../config.js') : { db };
-
+let config = (fs.statSync('config.js').isFile()) ? require('../config') : { db };
 let sequelize;
-try {
-  sequelize = new Sequelize(
-    `postgres://${config.db.username}:${config.db.password}@localhost:5432/${config.db.database}`,
-    {
-      operatorsAliases: false
-    }
-  );
-} catch (error) {
-  console.error(error);
-}
+  try {
+    sequelize = new Sequelize(
+      `postgres://${config.db.username}:${config.db.password}@localhost:5432/${config.db.database}`,
+      {
+        operatorsAliases: false
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+
 
 module.exports = {
   database: sequelize,
