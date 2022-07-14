cf2f8a33a264ad35b14c0fd1ee3cdc2993c9cec7
yoshifoxor <yshs.adachi@gmail.com>
2022-07-14 16:53:01 +0900

    Herokuの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -45,8 +45,11 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:8000/auth/github/callback',
-    }, function (accessToken, refreshToken, profile, done) {
+      callbackURL: process.env.HEROKU_URL
+        ? process.env.HEROKU_URL + 'auth/github/callback'
+        : 'http://localhost:8000/auth/github/callback',
+    },
+    function (accessToken, refreshToken, profile, done) {
       process.nextTick(function () {
         User.upsert({
           userId: profile.id,
