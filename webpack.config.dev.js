0bacf9cced497b534d1d79c041a3c629ef1b9d17
yoshifoxor <yshs.adachi@gmail.com>
2023-06-01 22:06:13 +0900

    Update package

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,4 @@
-const webpack = require('webpack');
-
+// const webpack = require('webpack');
 module.exports = {
   context: `${__dirname}/app`,
   mode: 'development',
@@ -13,12 +12,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          }],
       },
     ],
   },
@@ -29,5 +28,5 @@ module.exports = {
       'window.jQuery': 'jquery',
     }),
   ], */
-  //devtool: 'source-map',
+  devtool: 'source-map',
 };
