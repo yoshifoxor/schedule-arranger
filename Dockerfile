797d75db24b03f54dedbcee699ca7df2c8260498
yoshifoxor <yshs.adachi@gmail.com>
2024-05-21 14:22:03 +0900

    Router オブジェクトのテスト

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
-FROM --platform=linux/x86_64 node:14.17.6
-
-RUN sed -i 's@archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
+FROM --platform=linux/x86_64 node:20.12-slim
 
 RUN apt-get update \
-&& apt-get install --no-install-recommends -y locales tmux vim \
+&& apt-get install --no-install-recommends -y locales git procps vim tmux curl \
 && apt-get clean \
 && rm -rf /var/lib/apt/lists/*
 
 RUN locale-gen ja_JP.UTF-8
 RUN localedef -f UTF-8 -i ja_JP ja_JP
 
-ENV LANG ja_JP.UTF-8
-ENV TZ Asia/Tokyo
+ENV LANG=ja_JP.UTF-8
+ENV TZ=Asia/Tokyo
 
 WORKDIR /app
--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet')
-var session = require('express-session');
-var passport = require('passport');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('node:path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet')
+const session = require('express-session');
+const passport = require('passport');
 
 require('dotenv').config();
 
-var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = process.env.CLIENT_ID;
-var GITHUB_CLIENT_SECRET = process.env.CLIENT_SECRET;
+const GitHubStrategy = require('passport-github2').Strategy;
+const GITHUB_CLIENT_ID = process.env.CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 
 passport.use(new GitHubStrategy({
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
       callbackURL: 'http://localhost:8000/auth/github/callback',
-    },function (accessToken, refreshToken, profile, done) {
-      process.nextTick(function () {
+    }, (accessToken, refreshToken, profile, done) => {
+      process.nextTick(() => {
         return done(null, profile);
       });
     }
 ));
 
-var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var logoutRouter= require('./routes/logout')
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const logoutRouter= require('./routes/logout')
 
-var app = express();
+const app = express();
 app.use(helmet());
 
 // view engine setup
@@ -58,13 +53,12 @@ app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 
 app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] }),
-  function (req, res) {
-});
+  passport.authenticate('github', { scope: ['user:email'] })
+);
 
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
+  (req, res) => {
     res.redirect('/');
 });
 
